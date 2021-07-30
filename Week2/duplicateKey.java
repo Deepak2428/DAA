546863dc5cde7f882cb994d8b0ad4bdd7c91a3d3
Deepak2428 <81847091+Deepak2428@users.noreply.github.com>
2021-07-30 12:04:41 +0530

    Update duplicateKey.java

--- Week2/duplicateKey.java
+++ Week2/duplicateKey.java
@@ -1,1 +1,36 @@
 
+import java.util.*;
+class duplicateKey
+{
+  public static void main(String args[])
+  {
+    Scanner scn= new Scanner(System.in);
+    int n=scn.nextInt();
+    int a[]= new int[n];
+    for(int i=0;i<n;i++)
+    {
+      a[i]=scn.nextInt();
+    }
+    int key=scn.nextInt();
+    int l=0;
+    int u=n-1;
+    int fi=0;
+    while(l<=u)
+    {
+      int mid= l+(u-l)/2;
+      if(a[mid]==key)
+      {
+        fi=mid-1;
+      }
+      else if(a[mid]<key)
+      {
+        l=mid+1;
+      }
+      else
+      {
+        u=mid-1;
+      }
+    }
+    System.out.println(fi);
+  }
+}
