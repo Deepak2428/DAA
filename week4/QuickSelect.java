cfd3d456517df45fe932c8a55ae16a6889f1f45c
Deepak2428 <81847091+Deepak2428@users.noreply.github.com>
2021-08-26 11:40:35 +0530

    Update QuickSelect.java

--- week4/QuickSelect.java
+++ week4/QuickSelect.java
@@ -49,6 +49,8 @@ public class QuickSelect {
 
   public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
+	int t=scn.nextInt();
+	while(t>0){
     int n = scn.nextInt();
     int[] arr = new int[n];
     for (int i = 0; i < n; i++) {
@@ -56,6 +58,7 @@ public class QuickSelect {
     }
     int k = scn.nextInt();
     System.out.println(quickSelect(arr,0,arr.length - 1,k - 1));
+  }
   }
 
 }
