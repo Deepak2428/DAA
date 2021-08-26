d0be759d1363c6f79963f549018f88c28520c961
Deepak2428 <81847091+Deepak2428@users.noreply.github.com>
2021-08-26 11:38:38 +0530

    Update QuickSelect.java

--- week4/QuickSelect.java
+++ week4/QuickSelect.java
@@ -22,7 +22,6 @@ public class QuickSelect {
   }
 
   public static int partition(int[] arr, int pivot, int lo, int hi) {
-    //System.out.println("pivot -> " + pivot);
     int i = lo, j = lo;
     while (i <= hi) {
       if (arr[i] <= pivot) {
@@ -33,13 +32,11 @@ public class QuickSelect {
         i++;
       }
     }
-    //System.out.println("pivot index -> " + (j - 1));
     return (j - 1);
   }
 
-  // used for swapping ith and jth elements of array
+
   public static void swap(int[] arr, int i, int j) {
-    //System.out.println("Swapping " + arr[i] + " and " + arr[j]);
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
@@ -54,6 +51,8 @@ public class QuickSelect {
 
   public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
+    int t= scn.nextInt();
+    while(t>0){
     int n = scn.nextInt();
     int[] arr = new int[n];
     for (int i = 0; i < n; i++) {
@@ -61,6 +60,7 @@ public class QuickSelect {
     }
     int k = scn.nextInt();
     System.out.println(quickSelect(arr,0,arr.length - 1,k - 1));
+    }
   }
 
 }
