ceaf04bfb150ab6bccf891a1870a093fdb041a58
Deepak2428 <81847091+Deepak2428@users.noreply.github.com>
2021-09-27 20:27:08 +0530

    a

--- Week6/isBipartite.java
+++ Week6/isBipartite.java
@@ -1,6 +1,31 @@
 import java.util.*;
 class isBipartite
 {
-	public static void main(String args[])
-	{
-		
+    static class Edge
+    {
+        int v;
+        Edge(int v)
+        {
+            this.v=v;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner scn= new Scanner(System.in);
+        System.out.print("\n Enter no of vertices : ");
+        int vtces=scn.nextInt();
+        ArrayList<Edge> graph[]= new ArrayList [vtces];
+        for(int i=0;i<vtces;i++)
+        {
+            graph[i]=new ArrayList<>();
+        }
+        System.out.print("\n Enter no of edges : ");
+        int E= scn.nextInt();
+        
+        for(int i=0;i<E;i++)
+        {
+            int v1=scn.nextInt();
+            int v2=scn.nextInt();
+            graph[v1].add(new Edge(v1,v2));
+            graph[v2].add(new Edge(v2,v1));
+        }
