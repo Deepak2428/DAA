dd7aed5f8e342372e4f033d0f08bb20e66476a5c
Deepak2428 <81847091+Deepak2428@users.noreply.github.com>
2021-10-09 16:33:25 +0530

    Create maxProfit.java

---
+++ Week8/maxProfit.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class maxProfit
+{
+    static class Edge
+    {
+        int v;
+        int nbr;
+        int wt;
+        
+        Edge(int v,int nbr,int wt)
+        {
+            this.v=v;
+            this.nbr=nbr;
+            this.wt=wt;
+        }
+    }
+    
+    static class pair implements Comparable<pair>
+    {
+        int v;
+        int av;
+        int wt;
+        pair(int v,int av,int wt)
+        {
+            this.v=v;
+            this.av=av;
+            this.wt=wt;
+        }
+        
+        public int compareTo(pair o)
+        {
+            return this.wt- o.wt;
+        }
+    }
+    
